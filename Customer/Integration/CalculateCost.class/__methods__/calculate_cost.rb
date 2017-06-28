e79f6401d714abbcfde136bc6d2b7f0c8ca264d5
nik <supernoodz@users.noreply.github.com>
2017-06-28 20:48:42 +0100

    latest

--- Customer/Integration/CalculateCost.class/__methods__/calculate_cost.rb
+++ Customer/Integration/CalculateCost.class/__methods__/calculate_cost.rb
@@ -4,28 +4,14 @@
 
 $evm.root.attributes.sort.each { |k, v| $evm.log(:info, "\t Attribute: #{k} = #{v}")}
 
-exit MIQ_OK unless $evm.root.attributes.has_key?('dialog_template')
+# exit MIQ_OK unless $evm.root.attributes.has_key?('dialog_template')
 
 cpu_price      = $evm.object['cpu_price']                   # get os price per CPU from the instance
 mem_price      = $evm.object['mem_price']                   # get size price per GB from the instance
 storage_price  = $evm.object['storage_price']           # get storage price per GB from the instance
 
-# case $evm.root['vmdb_object_type']
-# when 'service_template'
-#   # Standard, single catalogue item request...
-
-#   service_template  = $evm.vmdb('service_template')
-#   resource          = service_template.service_resources.first.resource
-
-#   number_of_sockets = resource.options[:number_of_sockets].first.to_i
-#   cores_per_socket  = resource.options[:cores_per_socket].first.to_i
-#   cpu_count         = number_of_sockets * cores_per_socket
-
-#   mem_size          = resource.options[:vm_memory].first.to_i / 1024 # GB
-
-#   storage_size      = resource.vm_template.disk_1_size.to_i / (1024 * 1024 * 1024) # GB
-# else
-  # Bundle of VMs, as yet unspecified
+if $evm.root.attributes.has_key?('dialog_template')
+  # VM bundle
 
   number_of_vms = $evm.root['dialog_number_of_vms']
   number_of_vms ? number_of_vms = number_of_vms.to_i : (exit MIQ_OK)
@@ -45,14 +31,44 @@ storage_price  = $evm.object['storage_price']           # get storage price per 
   mem_size          = 2 # GB
   storage_size      = image.disk_1_size.to_i / (1024 * 1024 * 1024) # GB
 
-# end
+  number_of_vms = $evm.root['dialog_number_of_vms'].to_i
+
+else
+
+  case $evm.root['vmdb_object_type']
+  when 'service_template'
+    # Standard, single catalogue item request...
+
+    service_template = $evm.root['service_template']
+
+    # $evm.log(:info, service_template)
+    # $evm.log(:info, service_template.associations)
+
+    # service_template  = $evm.vmdb('service_template')
+    resource          = service_template.service_resources.first.resource
+
+    number_of_sockets = resource.options[:number_of_sockets].first.to_i
+    cores_per_socket  = resource.options[:cores_per_socket].first.to_i
+    cpu_count         = number_of_sockets * cores_per_socket
+
+    mem_size          = resource.options[:vm_memory].first.to_i / 1024 # GB
+
+    storage_size      = resource.vm_template.disk_1_size.to_i / (1024 * 1024 * 1024) # GB
+
+    number_of_vms = 1
+
+  else
+    exit MIQ_OK
+  end
+
+end
 
 # Calculate the cost for the CPU, memory, and storage
 cpu_cost     = cpu_price * cpu_count
 mem_cost     = mem_price * mem_size
 storage_cost = (storage_price * storage_size)
 
-total_cost = (cpu_cost + mem_cost + storage_cost) * $evm.root['dialog_number_of_vms'].to_i
+total_cost = (cpu_cost + mem_cost + storage_cost) * number_of_vms
 
 # Deteremine the total cost and round to two decimal points
 total_cost = sprintf("%.2f", total_cost.round(2))
