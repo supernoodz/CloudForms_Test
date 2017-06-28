c8be8bfb577b07d94fb8f9b2a5bfa327a2a6c16e
nik <supernoodz@users.noreply.github.com>
2017-06-28 18:36:50 +0100

    latest

--- Customer/Integration/Methods.class/__methods__/order_ansible_playbook.rb
+++ Customer/Integration/Methods.class/__methods__/order_ansible_playbook.rb
@@ -19,7 +19,7 @@ def exec_automation_request(playbook, host, credential)
   post_params = {
     :version => '1.1',
     :uri_parts => {
-      :namespace  => "ManageIQ/System",
+      :namespace  => "System",
       :class      => "Request",
       :instance   => "order_ansible_playbook"
     },
@@ -66,21 +66,31 @@ require 'rest-client'
 API_URI     = 'https://localhost/api'
 USER        = 'admin'
 PASSWORD    = 'smartvm'
-@auth_token = get_auth
 
 prov = $evm.root["miq_provision"]
-# host = prov.vm.ipaddresses[1]
-host = prov.vm.ipaddresses[0]
 
-# install_app = prov.options[:ws_values][:install_app]
-install_app = "Vodafone All-In-One"
+ip_list = prov.vm.ipaddresses
+$evm.log(:info, ip_list)
 
-# exit MIQ_OK if install_app.nil? || install_app == 'none'
+if ip_list.empty?
+  $evm.root['ae_result']      = 'retry'
+  $evm.root['ae_retry_limit'] = 1.minute
+else
+  @auth_token = get_auth
 
-# Default VMware machine credentials
-name = "VMware (Machine) 2"
+  # host = prov.vm.ipaddresses[1]
+  host = prov.vm.ipaddresses[0]
 
-credential = $evm.vmdb('ManageIQ_Providers_AutomationManager_Authentication').find_by(:name => name).id
-raise 'Credentials not found' if credential.nil?
+  # install_app = prov.options[:ws_values][:install_app]
+  install_app = "Vodafone All-In-One"
 
-request_id = exec_automation_request(install_app, host, credential)
+  # exit MIQ_OK if install_app.nil? || install_app == 'none'
+
+  # Default VMware machine credentials
+  name = "VMware (Machine) 2"
+
+  credential = $evm.vmdb('ManageIQ_Providers_AutomationManager_Authentication').find_by(:name => name).id
+  raise 'Credentials not found' if credential.nil?
+
+  request_id = exec_automation_request(install_app, host, credential)
+end
